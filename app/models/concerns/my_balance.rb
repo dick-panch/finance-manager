5a541ab394511c9a5c818c4131541ac7572c9da4
Dick Panch <dick.panch@gmail.com>
2017-09-02 01:27:17 +0530

    Fixed issue of my balance graph

--- app/models/concerns/my_balance.rb
+++ app/models/concerns/my_balance.rb
@@ -4,16 +4,17 @@ module MyBalance
 		balance = user.balances.find_by(month: transaction.month, year: transaction.year)
 		total_incomes = user.transactions.where("month = ? and year = ?", transaction.month, transaction.year).incomes.map{|t| t.amount}.sum.to_f.round(2)
 		total_expenses = user.transactions.where("month = ? and year = ?", transaction.month, transaction.year).expenses.map{|t| t.amount}.sum.to_f.round(2)
+		total_investments = user.transactions.where("month = ? and year = ?", transaction.month, transaction.year).investments.map{|t| t.amount}.sum.to_f.round(2)
 
 		if balance.present?
 			balance.update({
-				amount: (total_incomes - total_expenses)
+				amount: (total_incomes - (total_expenses + total_investments))
 			})
 		else
 			user.balances.create({
 				month: transaction.month,
 				year: transaction.year,
-				amount: (total_incomes - total_expenses)
+				amount: (total_incomes - (total_expenses + total_investments))
 			})
 		end
 	end
