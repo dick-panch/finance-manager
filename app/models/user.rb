b28f6141ad7fd32e7b08a903cc049961f8b210bd
Dick Panch <dick.panch@gmail.com>
2017-11-25 15:33:08 +0530

    Added Scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,8 @@ class User < ApplicationRecord
   ## Callback
   after_create :create_default_setting
 
+  ## Scopes
+  scope :my_users, -> {where('users.role_id = ?', 1)}
   ## Callback Methods
   def create_default_setting
   	self.create_setting
@@ -27,5 +29,9 @@ class User < ApplicationRecord
 
   def is_admin?
   	self.role_id == 2
+  end
+
+  def role
+    Rails.application.secrets.roles[role_id]
   end
 end
